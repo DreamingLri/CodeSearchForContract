111dad4c00d3ed30fd0f6e04d64ec6d37debc0e4
DreamingLri <3507415455@qq.com>
2024-03-02 15:18:17 +0800

    add title and input-box

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,8 +2,19 @@
 
 import Title from "@/components/Title.vue";
 import {ref} from "vue";
+import request from "@/utils/request";
+
 
 const textarea = ref('')
+const text = ref()
+
+function getContract() {
+  request.get("https://api.etherscan.io/api?module=contract&action=getsourcecode&address="+textarea.value+"&apikey=7H5I68STDR37JYFKW9KSHK76NXUG4DZRHW").then(res=>{
+    text.value =  res.result[0].SourceCode
+    console.log(text.value)
+  })
+}
+
 </script>
 
 <template>
@@ -19,6 +30,12 @@ const textarea = ref('')
             type="textarea"
             placeholder="请输入要查询的代码"
         />
+        <el-button type="primary" @click="getContract">Primary</el-button>
+        <el-scrollbar height="300px">
+          <el-card style="max-width: 100%">
+            <pre style="text-align: left">{{text}}</pre>
+          </el-card>
+        </el-scrollbar>
 
       </el-main>
     </el-container>
