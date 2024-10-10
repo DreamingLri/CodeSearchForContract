00eece6bf611e33188af984ec2862d5134250db8
DreamingLri <3507415455@qq.com>
2024-10-10 16:45:44 +0800

    ✨ add element icon

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import hljs from 'highlight.js' //导入代码高亮文件
-import 'highlight.js/styles/github.css' 
+import 'highlight.js/styles/github.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App)
-    .directive('highlight', function (el) {
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.directive('highlight', function (el) {
     const blocks = el.querySelectorAll('pre code');
     blocks.forEach((block: any) => {
         hljs.highlightBlock(block)
