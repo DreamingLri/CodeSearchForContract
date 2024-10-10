f6f05b2aa82ed153555dbaafef701394f93cdc41
DreamingLri <3507415455@qq.com>
2024-10-10 16:22:04 +0800

    add code search and code showcase

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,107 @@
 <script setup lang="ts">
+import { nextTick, ref, watch } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+import request from "../utils/request";
+import { ElMessage } from "element-plus";
+
+const search_code = ref('')
+const search_result = ref()
+const selected_code = ref([])
+
+function getContract() {
+  request.get("https://api.etherscan.io/api?module=contract&action=getsourcecode&address=" + search_code.value + "&apikey=7H5I68STDR37JYFKW9KSHK76NXUG4DZRHW").then(res => {
+    if (res.data.message === 'OK') {
+      search_result.value = res.data
+      formatToList(search_result.value?.result[0].SourceCode)
+    } else {
+      ElMessage.warning(res.data.result)
+    }
+  })
+}
+
+interface FileContent {
+  content: string;
+}
+
+interface FileObject {
+  [key: string]: FileContent;
+}
+const contract_list = ref<{ key: string; value: any }[]>([])
+function formatToList(text: any) {
+  let list = text.slice(1, -1)
+  let json: FileObject = JSON.parse(list)
+  for (const [key, value] of Object.entries(json.sources)) {
+    let obj = {
+      key: key,
+      value: value.content
+    }
+    const exists = contract_list.value.some((item) => item.key === obj.key);
+    if (!exists) {
+      contract_list.value.push(obj);
+    }
+  }
+  console.log(contract_list.value);
+}
 </script>
 
 <template>
-    <h1>
-      Hello
-    </h1>
+  <div class="common-layout">
+    <el-container style="height: 100vh">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="400px" style="padding: 0 20px">
+          <div>
+            <div>
+              <p>Contract Address</p>
+              <el-input v-model="search_code" style="max-width: 600px" placeholder="0x.." class="input-with-select">
+                <template #append>
+                  <el-button :icon="Search" @click="getContract" />
+                </template>
+              </el-input>
+            </div>
+            <div v-if="search_result?.message === 'OK'">
+              <p>Choose Contract Files</p>
+              <el-scrollbar max-height="400px">
+                <el-card style="border-radius: 10px" shadow="never">
+                  <el-checkbox-group v-model="selected_code">
+                    <el-checkbox v-for="item in contract_list" :key="item.value" :label="item.key" :value="item.value">
+                      <el-popover placement="top-start" trigger="hover" :content="item.key">
+                        <template #reference>
+                          {{ item.key }}
+                        </template>
+                      </el-popover>
+                    </el-checkbox>
+                  </el-checkbox-group>
+                </el-card>
+              </el-scrollbar>
+            </div>
+          </div>
+        </el-aside>
+        <el-main style="padding: 0 20px">
+          <p>Contract Code</p>
+            <el-card>
+              <el-scrollbar max-height="500px">
+              <div v-if="selected_code">
+                <div v-for="code in selected_code" :key="code" v-highlight>
+                  <pre style="white-space: pre-wrap;"><code>{{ code }}</code></pre>
+                </div>
+              </div>
+              </el-scrollbar>
+            </el-card>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
+.word-break {
+  margin-bottom: 20px;
+  word-wrap: break-word;
+  /* 确保长单词或长字符串可以在容器宽度内换行 */
+  word-break: break-all;
+  /* 如果需要，可以在任何地方打断并换行 */
+  white-space: normal;
+  /* 让文本正常换行 */
+}
 </style>
