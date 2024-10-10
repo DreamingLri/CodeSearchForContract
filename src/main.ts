da79b4232b237d02ef7dab9ad1780d0fd384565a
DreamingLri <3507415455@qq.com>
2024-10-10 16:20:18 +0800

    ✨ add heighlightjs

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import hljs from 'highlight.js' //导入代码高亮文件
+import 'highlight.js/styles/github.css' 
 
 createApp(App)
+    .directive('highlight', function (el) {
+    const blocks = el.querySelectorAll('pre code');
+    blocks.forEach((block: any) => {
+        hljs.highlightBlock(block)
+    })
+})
     .use(router)
     .use(ElementPlus)
     .mount('#app')
